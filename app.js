5ae79c0f8d68fab57cb587aad7d50a9c64b84d5b
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-07-01 11:52:39 -0600

    Pug and Template engine added.

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-//console.log('Hello World!');
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-})
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+// Home Route
+app.get('/', function(req, res) {
+  let articles = [{
+      id: 1,
+      title: 'Article one',
+      author: 'Raghav Sharma',
+      body: 'This is the first article',
+    },
+    {
+      id: 2,
+      title: 'Article two',
+      author: 'John Doe',
+      body: 'This is the second article',
+    },
+    {
+      id: 3,
+      title: 'Article three',
+      author: 'Jane Doe',
+      body: 'This is the third  article',
+    },
+  ]
+  res.render('index', {
+    title: 'Articles',
+    articles: articles,
+  });
+});
+
+// Adding Arcticles Route
+app.get('/articles/add', function(req, res) {
+  res.render('add_article', {
+    title: 'Add Articles',
+  });
+});
+
+// Start Server
 app.listen(3000, function() {
   console.log('Server started on port 3000...');
-})
+});
