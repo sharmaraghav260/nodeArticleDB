b318b9e1a615139467c4c9dcdd11a46287b2daeb
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-07-01 13:49:07 -0600

    Mongoose and Setup model done.
    Worked with the MongoDB database and with the Mongoose ORM to fetch data from the database.

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/articleDB');
+
+let db = mongoose.connection;
+
+// Check Connection
+db.once('open', function() {
+  console.log('Connected to MongoDB...');
+})
+
+// Check for DB errors
+db.on('error', function(err) {
+  console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,28 +28,34 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', function(req, res) {
-  let articles = [{
-      id: 1,
-      title: 'Article one',
-      author: 'Raghav Sharma',
-      body: 'This is the first article',
-    },
-    {
-      id: 2,
-      title: 'Article two',
-      author: 'John Doe',
-      body: 'This is the second article',
-    },
-    {
-      id: 3,
-      title: 'Article three',
-      author: 'Jane Doe',
-      body: 'This is the third  article',
-    },
-  ]
-  res.render('index', {
-    title: 'Articles',
-    articles: articles,
+  // let articles = [{
+  //     id: 1,
+  //     title: 'Article one',
+  //     author: 'Raghav Sharma',
+  //     body: 'This is the first article',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Article two',
+  //     author: 'John Doe',
+  //     body: 'This is the second article',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Article three',
+  //     author: 'Jane Doe',
+  //     body: 'This is the third  article',
+  //   },
+  // ]
+  Article.find({}, function(err, articles) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles,
+      });
+    }
   });
 });
 
