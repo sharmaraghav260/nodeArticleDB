18c6f0b4163b04ddf3a3ff91b50a295ff932881e
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-07-02 13:08:18 -0600

    Flash messaging and Validation Added.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/articleDB');
@@ -40,6 +43,38 @@ app.use(bodyParser.json())
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
 // Home Route
 app.get('/', function(req, res) {
   Article.find({}, function(err, articles) {
@@ -54,83 +89,9 @@ app.get('/', function(req, res) {
   });
 });
 
-// Get Single Article
-app.get('/article/:id', function(req, res) {
-  Article.findById(req.params.id, function(err, article) {
-    res.render('article', {
-      article: article,
-    });
-  });
-});
-
-// Adding Arcticles Route
-app.get('/articles/add', function(req, res) {
-  res.render('add_article', {
-    title: 'Add Article',
-  });
-});
-
-// Adding Submit POST Route
-app.post('/articles/add', function(req, res) {
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  article.save(function(err) {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  })
-});
-
-// Load Edit Form
-app.get('/article/edit/:id', function(req, res) {
-  Article.findById(req.params.id, function(err, article) {
-    res.render('edit_article', {
-      title: 'Edit the Article',
-      article: article,
-    });
-  });
-});
-
-// Update Submit POST Route
-app.post('/articles/edit/:id', function(req, res) {
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = {
-    _id: req.params.id
-  }
-
-  Article.update(query, article, function(err) {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  })
-});
-
-// Deleting Article
-app.delete('/article/:id', function(req, res) {
-  let query = {
-    _id: req.params.id
-  }
-
-  Article.remove(query, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    res.send('Success: Article Deleted');
-  })
-});
+// Router Files
+let articles = require('./routes/articles');
+app.use('/articles', articles);
 
 // Start Server
 app.listen(3000, function() {
